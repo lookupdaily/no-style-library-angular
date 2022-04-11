7e10a7c2fa6c21c179361b1c3eeaf5fe674da92f
Liz Daly <liz7daly@gmail.com>
2022-04-11 18:08:50 +0100

    Add test input component in starter app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import { NSErrorMessage } from 'projects/no-style/src/lib/input/input.types';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'no-styles-angular';
+
+  form: FormGroup =  new FormGroup({
+    test: new FormControl({value: 'Nancy', disabled: true}),
+    test1: new FormControl('', Validators.max(10)),
+  });
+
+  errorMessages: NSErrorMessage[] = [{
+    name: 'max',
+    text: 'Value should be less than 10 characters',
+    showErrorOnChange: true,
+  }];
+
+  formControl(name: string): AbstractControl {
+    return this.form.get(name) as AbstractControl;
+  }
+
+  ngOnInit(): void {
+    this.form.valueChanges.subscribe(() => {
+      console.log(this.form);
+    })
+  }
 }
