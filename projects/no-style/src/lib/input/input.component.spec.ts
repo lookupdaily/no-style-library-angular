87da151f57f247b73e4a391719acfd78ca2cbd0c
Liz Daly <liz7daly@gmail.com>
2022-04-11 16:56:05 +0100

    Add select type to input component

--- projects/no-style/src/lib/input/input.component.spec.ts
+++ projects/no-style/src/lib/input/input.component.spec.ts
@@ -1,4 +1,4 @@
-import { Component, NO_ERRORS_SCHEMA, ViewChild } from '@angular/core';
+import { Component, NO_ERRORS_SCHEMA, SimpleChange, ViewChild } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
@@ -10,6 +10,10 @@ describe('InputComponent', () => {
   let component: TestReactiveFormHostComponent;
   let fixture: ComponentFixture<TestReactiveFormHostComponent>;
   let inputComponent: InputComponent;
+
+  const createSimpleChange = (newValue: any): SimpleChange => {
+    return new SimpleChange(null, newValue, true)
+  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -43,6 +47,32 @@ describe('InputComponent', () => {
 
     const label = fixture.debugElement.query(By.css('label'));
     expect(label.nativeElement.innerHTML).toContain('Address');
+  });
+
+    describe('When select type is specified', () => {
+    beforeEach(() => {
+      inputComponent.type = 'select';
+      inputComponent.ngOnChanges({type: createSimpleChange(inputComponent.type)});
+      fixture.detectChanges();
+    });
+
+    it('renders select if input type is select', () => {
+      const select = fixture.debugElement.query(By.css('select'));
+      expect(select).toBeTruthy();
+    });
+
+    it ('renders a list of given options', () => {
+      const options = [
+        { value: '1', label: 'Option 1' },
+        { value: '2', label: 'Option 2'},
+      ];
+      inputComponent.options = options;
+      fixture.detectChanges();
+      const optionsElements = fixture.debugElement.queryAll(By.css('option'));
+
+      expect(optionsElements.length).toBe(2);
+
+    });
   });
 });
 
